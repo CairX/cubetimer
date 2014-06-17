da0939347bd029091f1acffd66a4db3bf5ac343f
Teodor Lagerqvist <git@xio.se>
2014-06-17 22:34:03 +0200

    flexbox styling

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-var i;
+var interval;
 var time = document.getElementById('time');
 var timer = new Timer();
+var KEY_SPACE = 32;
 
 function update() {
 	time.innerHTML = TimeFormatter.format(timer.current(), '%%M:%S:%h');
 }
 
-window.addEventListener("keypress", function(e) {
-	if (e.charCode === 32) toggleTimer();
-}, false);
+window.addEventListener("keypress", function(e){if(e.charCode === KEY_SPACE) toggleTimer();}, false);
 window.addEventListener("click", toggleTimer, false);
-
 
 function toggleTimer() {
 	if (timer.isRunning()) {
-		window.clearInterval(i);
+		window.clearInterval(interval);
 		timer.stop();
 		time.innerHTML = TimeFormatter.format(timer.time(), '%%M:%S:%h');
 	} else {
 		timer.start();
-		i = window.setInterval(update, 10);
+		interval = window.setInterval(update, 10);
 	}
 };

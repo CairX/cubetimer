56e0ac3ca1207ebfb0eac5e99b9b511835d93501
cairns <lazycairx@gmail.com>
2014-06-22 00:34:27 +0200

    Made some of the code in the TimeFormatter easier to read. Replaced a few tabs with spaces.

--- TimeFormatter.js
+++ TimeFormatter.js
@@ -1,47 +1,60 @@
 function TimeFormatter() {}
-TimeFormatter.format = function(milliseconds, format) {
-    if (milliseconds == null || format == null) { return ''; }
+TimeFormatter.format = function (milliseconds, format) {
+    if (milliseconds === null || format === null) { return ''; }
 	
 	var negative = milliseconds<0;
-	if(negative) {
-		milliseconds*=-1;
+	if (negative) {
+		milliseconds *= -1;
 	}
 
     // TODO Remove duplicate results.
     var directives = format.match(/([%]{1,2}[DHMSfh]{1})/g);
-    if (directives != null) {
-        for (var i = 0; i < directives.length; i++) {
+    var i;
+    if (directives !== null) {
+        for (i = 0; i < directives.length; i++) {
             format = TimeFormatter.replace(milliseconds, directives[i], format);
         }
     }
 	
-    return (negative? "-":"") + format;
+    return (negative ? '-' : '') + format;
 };
-TimeFormatter.replace = function(milliseconds, directive, s) {
+TimeFormatter.replace = function (milliseconds, directive, s) {
+    var value = '';
     switch (directive) {
         case '%%D':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 86400000), 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 86400000), 2);
+            break;
         case '%H':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 3600000) % 24, 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 3600000) % 24, 2);
+            break;
         case '%%H':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 3600000), 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 3600000), 2);
+            break;
         case '%M':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 60000) % 60, 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 60000) % 60, 2);
+            break;
         case '%%M':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 60000), 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 60000), 2);
+            break;
         case '%S':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 1000) % 60, 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 1000) % 60, 2);
+            break;
         case '%%S':
-            return s.replace(directive, TimeFormatter.zero(Math.floor(milliseconds / 1000), 2));
+            value = TimeFormatter.zero(Math.floor(milliseconds / 1000), 2);
+            break;
         case '%f':
-            return s.replace(directive, TimeFormatter.zero(milliseconds % 1000, 3));
+            value = TimeFormatter.zero(milliseconds % 1000, 3);
+            break;
         case '%%f':
-            return s.replace(directive, TimeFormatter.zero(milliseconds, 3));
+            value = TimeFormatter.zero(milliseconds, 3);
+            break;
         case '%h':
-            return s.replace(directive, String(TimeFormatter.zero(milliseconds % 1000, 3)));
+            value = String(TimeFormatter.zero(milliseconds % 1000, 3)).substring(0, 2);
+            break;
     }
+    return s.replace(directive, value);
 };
-TimeFormatter.zero = function(s, length) {
+TimeFormatter.zero = function (s, length) {
     var d = length > s.toString().length ? length - s.toString().length : 0;
     for (var i = 0; i < d; i++) {
         s = '0' + s;
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+/*global navigator, window */
+
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 function vibrate(ms) {
-	if(navigator.vibrate) {
-		navigator.vibrate(ms);
-	}
+    if (navigator.vibrate) {
+        navigator.vibrate(ms);
+    }
 }
-
 
 var interval;
 var time = document.getElementById('time');
@@ -14,9 +15,9 @@ var selUsers = document.getElementById('selUsers');
 var selCountdown = document.getElementById('selCountdown');
 
 var pages = {
-	"settings": document.getElementById('pageSettings'),
-	"timer": document.getElementById('pageTimer'),
-	"result": document.getElementById('pageResult')
+    "settings": document.getElementById('pageSettings'),
+    "timer": document.getElementById('pageTimer'),
+    "result": document.getElementById('pageResult')
 };
 
 var btnInitTimer = document.getElementById('btnInitTimer');
@@ -30,11 +31,11 @@ btnSettings.addEventListener("click", showSettings, false);
 
 var state;
 var STATE_SETTINGS=0,
-	STATE_WAIT_FOR_COUNTDOWN=1,
-	STATE_WAIT_FOR_TIMER=2,
-	STATE_COUNTDOWN=3,
-	STATE_TIMER=4,
-	STATE_RESULT=5
+    STATE_WAIT_FOR_COUNTDOWN=1,
+    STATE_WAIT_FOR_TIMER=2,
+    STATE_COUNTDOWN=3,
+    STATE_TIMER=4,
+    STATE_RESULT=5
 ;
 
 var KEY_SPACE = 32;
@@ -50,178 +51,175 @@ showSettings();
 
 
 function showSettings() {
-	loadUsers();
-	loadEvents();
-	showPage("settings");
-	state = STATE_SETTINGS;
+    loadUsers();
+    loadEvents();
+    showPage("settings");
+    state = STATE_SETTINGS;
 }
 
 
 function eventHandler(e) {
-	
-	switch(state) {
-	
-		case STATE_WAIT_FOR_COUNTDOWN:
-		if((e.type==="keydown" && e.keyCode===KEY_SPACE) || (e.type==="touchstart")) {
-			state = STATE_COUNTDOWN;
-			upBetween = false;
-			Timer.start();
-			interval = window.setInterval(function() {
-				updateTime(Timer.getTime());
-				
-				if(Timer.getTime()>=0) {
-					console.debug("force start timer");
-					clearInterval(interval);
-					startTimer();
-					vibrate(100);
-				}
-			}, 10);
-		}
-		break;
-		
-		case STATE_COUNTDOWN:
-		if((e.type==="keyup" && e.keyCode===KEY_SPACE) || (e.type==="touchend")) {
-			console.debug("start timer");
-			clearInterval(interval);
-			console.debug("start timer");
-			startTimer();
-			upBetween = true;
-		}
-		break;
-		
-		case STATE_WAIT_FOR_TIMER:
-		if((e.type==="keydown" && e.keyCode===KEY_SPACE) || (e.type==="touchstart")) {
-			upBetween = false;
-			console.debug("start timer!");				
-			startTimer();
-		}
-		break;
-		
-		case STATE_TIMER:
-		if(upBetween && ((e.type==="keydown" && e.keyCode===KEY_SPACE) || e.type==="touchstart")) {
-			e.preventDefault();
-			stopTimer();
-		} else if(!upBetween && ((e.type==="keyup" && e.keyCode===KEY_SPACE) || e.type==="touchend")) {
-			upBetween = true;
-		}
-		break;
-	}
+    switch(state) {
+        case STATE_WAIT_FOR_COUNTDOWN:
+        if((e.type==="keydown" && e.keyCode===KEY_SPACE) || (e.type==="touchstart")) {
+            state = STATE_COUNTDOWN;
+            upBetween = false;
+            Timer.start();
+            interval = window.setInterval(function() {
+                updateTime(Timer.getTime());
+                
+                if(Timer.getTime()>=0) {
+                    console.debug("force start timer");
+                    clearInterval(interval);
+                    startTimer();
+                    vibrate(100);
+                }
+            }, 10);
+        }
+        break;
+        
+        case STATE_COUNTDOWN:
+        if((e.type==="keyup" && e.keyCode===KEY_SPACE) || (e.type==="touchend")) {
+            console.debug("start timer");
+            clearInterval(interval);
+            console.debug("start timer");
+            startTimer();
+            upBetween = true;
+        }
+        break;
+        
+        case STATE_WAIT_FOR_TIMER:
+        if((e.type==="keydown" && e.keyCode===KEY_SPACE) || (e.type==="touchstart")) {
+            upBetween = false;
+            console.debug("start timer!");              
+            startTimer();
+        }
+        break;
+        
+        case STATE_TIMER:
+        if(upBetween && ((e.type==="keydown" && e.keyCode===KEY_SPACE) || e.type==="touchstart")) {
+            e.preventDefault();
+            stopTimer();
+        } else if(!upBetween && ((e.type==="keyup" && e.keyCode===KEY_SPACE) || e.type==="touchend")) {
+            upBetween = true;
+        }
+        break;
+    }
 }
 
 function startTimer() {
-	state = STATE_TIMER;
-	time.classList.remove("countdown");
-	Timer.reset();
-	Timer.start();
-	interval = window.setInterval(function() {
-		updateTime(Timer.getTime());
-	}, 10);
+    state = STATE_TIMER;
+    time.classList.remove("countdown");
+    Timer.reset();
+    Timer.start();
+    interval = window.setInterval(function() {
+        updateTime(Timer.getTime());
+    }, 10);
 }
 
 function stopTimer() {
-	Timer.stop();
-	clearInterval(interval);
-	state = STATE_RESULT;
-	result.innerHTML = TimeFormatter.format(Timer.getTime(), '%%M:%S.%h');
-	showPage("result");
-	
+    Timer.stop();
+    clearInterval(interval);
+    state = STATE_RESULT;
+    result.innerHTML = TimeFormatter.format(Timer.getTime(), '%%M:%S.%h');
+    showPage("result");
+    
 }
 
 
 function initTimer() {
-	showPage("timer");
-	
-	if(selCountdown.value>0) {
-		state = STATE_WAIT_FOR_COUNTDOWN;
-		time.classList.add("countdown");
-		Timer.reset(-parseInt(selCountdown.value));
-		updateTime(-parseInt(selCountdown.value));
-	} else {
-		state = STATE_WAIT_FOR_TIMER;
-		time.classList.remove("countdown");
-		Timer.reset();
-		updateTime(0);
-	}
+    showPage("timer");
+    
+    if(selCountdown.value>0) {
+        state = STATE_WAIT_FOR_COUNTDOWN;
+        time.classList.add("countdown");
+        Timer.reset(-parseInt(selCountdown.value));
+        updateTime(-parseInt(selCountdown.value));
+    } else {
+        state = STATE_WAIT_FOR_TIMER;
+        time.classList.remove("countdown");
+        Timer.reset();
+        updateTime(0);
+    }
 }
 
 
 function showPage(page) {
-	var sections = document.querySelectorAll("section");
-	for(var i=0; i<sections.length; i++) {
-		sections[i].style.display = "none";
-	}
-	pages[page].style.display = "block";
+    var sections = document.querySelectorAll("section");
+    for(var i=0; i<sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+    pages[page].style.display = "block";
 }
 
 
 
 function updateTime(ms) {
-	time.innerHTML = TimeFormatter.format(ms, '%%M:%S.%h');
+    time.innerHTML = TimeFormatter.format(ms, '%%M:%S.%h');
 }
 
 
 function loadUsers() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("get", "http://api.fruruf.se/get/users", true);
-	xhr.responseType = "json";
-	xhr.onload = loadUsersCallback;
-	xhr.send();
-	console.debug("load users from fruruf");
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "http://api.fruruf.se/get/users", true);
+    xhr.responseType = "json";
+    xhr.onload = loadUsersCallback;
+    xhr.send();
+    console.debug("load users from fruruf");
 }
 
 function loadUsersCallback(e) {
-	if(e.target.status === 200 && e.target.response.status === 1000) {
-		var json = e.target.response;
-		console.debug("users loaded", json);
-		for(var userId in json.users) {
-			var option = document.createElement("OPTION");
-			option.textContent = json.users[userId].username;
-			selUsers.appendChild(option);
-		}
-	} else {
-		console.error("Error loading users", e.target);
-	}
+    if(e.target.status === 200 && e.target.response.status === 1000) {
+        var json = e.target.response;
+        console.debug("users loaded", json);
+        for(var userId in json.users) {
+            var option = document.createElement("OPTION");
+            option.textContent = json.users[userId].username;
+            selUsers.appendChild(option);
+        }
+    } else {
+        console.error("Error loading users", e.target);
+    }
 }
 
 function loadEvents() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("get", "http://api.fruruf.se/get/events", true);
-	xhr.responseType = "json";
-	xhr.onload = loadEventsCallback;
-	xhr.send();
-	console.debug("load events from fruruf");
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "http://api.fruruf.se/get/events", true);
+    xhr.responseType = "json";
+    xhr.onload = loadEventsCallback;
+    xhr.send();
+    console.debug("load events from fruruf");
 }
 
 function loadEventsCallback(e) {
-	if(e.target.status === 200 && e.target.response.status === 1000) {
-		var json = e.target.response;
-		console.debug("events loaded", json);
-		for(var eventId in json.events) {
-			var option = document.createElement("OPTION");
-			option.textContent = json.events[eventId].name;
-			selEvents.appendChild(option);
-		}
-	} else {
-		console.error("Error loading events", e.target);
-	}
+    if(e.target.status === 200 && e.target.response.status === 1000) {
+        var json = e.target.response;
+        console.debug("events loaded", json);
+        for(var eventId in json.events) {
+            var option = document.createElement("OPTION");
+            option.textContent = json.events[eventId].name;
+            selEvents.appendChild(option);
+        }
+    } else {
+        console.error("Error loading events", e.target);
+    }
 }
 
 
-function submitToFRURUF() {		
-	var formData = new FormData();
-	formData.append("key", "fWG345wgQqnj");
-	formData.append("username", "test");
-	formData.append("time", timer.time());
-	formData.append("event", "333oh");		
+function submitToFRURUF() {     
+    var formData = new FormData();
+    formData.append("key", "fWG345wgQqnj");
+    formData.append("username", "test");
+    formData.append("time", timer.time());
+    formData.append("event", "333oh");      
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("post", "http://api.fruruf.se/add", true);
-	xhr.onload = timeSubmitted;
-	xhr.send(formData);
-	console.debug("Submit time to fruruf");
+    var xhr = new XMLHttpRequest();
+    xhr.open("post", "http://api.fruruf.se/add", true);
+    xhr.onload = timeSubmitted;
+    xhr.send(formData);
+    console.debug("Submit time to fruruf");
 }
 
 function timeSubmitted(e) {
-	console.debug("timeSubmitted", e.target.response);
+    console.debug("timeSubmitted", e.target.response);
 }
-
